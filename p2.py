461772eaaee9374dbe0565a2bdce2c1997a50d58
Baruch Mettler <bmettler@Baruchs-MacBook-Air.local>
2017-09-30 12:23:37 -0700

    third update

--- p2.py
+++ p2.py
@@ -32,8 +32,39 @@ print filename.split(".")
 #['c:', 'win', 'sys', 'met.exe']
 #['c:\\win\\sys\\met', 'exe']
 
+#String concatenation 
+mail = "b" + "@" + "mail.com"
+print mail
+mail += " this is an email"
+print mail
+mail = "".join([mail," - and it is a string"])
+print mail
 
-#I think this is a tuple 
+#b@mail.com
+#b@mail.com this is an email
+#b@mail.com this is an email - and it is a string
+
+#lists
+alist = []
+alist.append("brandon")
+print alist
+alist.append("weston")
+print alist 
+alist.append("raegan")
+alist.remove("weston")
+alist.append("dog")
+alist.append("cat")
+print alist
+print alist[1]
+alist.sort()
+print alist
+blist= ["house","car","bike"]
+newlist = alist + blist
+print newlist
+
+
+#I think this is a tuple .. it is. 
+# Once defined, it cant be changed
 tup = ("a","b","c","d")
 
 for mystr in tup:
@@ -61,6 +92,15 @@ def myfunc2(anotherint):
 	#lets just try to print thisint
 	print "thisint[" + str(thisint) +"]"
 
+def doerror(someval):
+	try:
+		x = int(someval)
+	except Exception as e:
+		print "oops.. [" + str(e) +"]"
+		return
+	print "just to see if I get here, i shouldnt if error"
+doerror("asdasdasd")
+	
 print "Function returned: [" + str(myfunction(1234)) + "]"
 
 if (3 in ar) and 4 in ar:	 print "3 and 4 is in our array"
@@ -68,6 +108,10 @@ if (3 in ar) and 4 in ar:	 print "3 and 4 is in our array"
 if 16 not in ar: print "16 isnt in the ar"
 
 if "c" in "hffrdsaxfcjh" and "z" not in "hffrdsaxfcjh": print "we found c, but not z"
+
+#
+
+
 
 ## fully figure out lists and tuples.. and dictionaries 
 
@@ -86,7 +130,44 @@ print hex(1) ## just returns 0x1.. need to figire out how to do this for other t
 
 print ord("A") ## get 65.. not sure, thought it would be a 41
 
-	
+import os
+# or import os as * 
+# potential namespace issues.. but dont have to write os.whatever
+
+print str(os.geteuid()) 
+print os.listdir("./")
+print os.stat("./file.txt") 
+print os.system("ls")
+
+osfiles = os.popen("find -f /tmp/*")
+print osfiles.readline()
+
+import sys
+
+print "show my sys"
+print sys.platform  
+# only wiwnsows... sys.getwindowsversion()
+print >> sys.stderr, "Error.. blah"
+
+
+drrfile = open("/etc/passwd")
+for line in drrfile:
+	#print line
+	username = line.split(":")
+	if len(username)> 1: 
+		#print int(username[2])
+		if ( int(username[2]) == 0): 
+			print "priv user !!!!!!!!!!!!!!!!!!!!!!!!!!!!" + username[0]
+
+#grab an argument 
+
+if sys.argv[1:]:
+	print "got arguments"
+	for myarg in sys.argv:
+		print myarg
+
+
+sys.exit()
 
 
 
